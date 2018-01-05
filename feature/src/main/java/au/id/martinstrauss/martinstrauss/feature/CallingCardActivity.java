883061fc2c69992cf29c4a185b40b94b7ec7a958
Martin Strauss <martinstrauss@google.com>
2018-01-05 15:47:55 -0800

    add animations

--- feature/src/main/java/au/id/martinstrauss/martinstrauss/feature/CallingCardActivity.java
+++ feature/src/main/java/au/id/martinstrauss/martinstrauss/feature/CallingCardActivity.java
@@ -1,6 +1,7 @@
 package au.id.martinstrauss.martinstrauss.feature;
 
 import android.annotation.SuppressLint;
+import android.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -31,9 +32,14 @@ public class CallingCardActivity extends AppCompatActivity {
    * and a change of the status and navigation bar.
    */
   private static final int UI_ANIMATION_DELAY = 300;
+
+  /**
+   * Whether or not we're showing the back of the card (otherwise showing the front).
+   */
+  private boolean mShowingBack = false;
+
   private final Handler mHideHandler = new Handler();
   private View mContentView;
-  private View mContactDetailsView;
   private final Runnable mHidePart2Runnable = new Runnable() {
     @SuppressLint("InlinedApi")
     @Override
@@ -64,35 +70,16 @@ public class CallingCardActivity extends AppCompatActivity {
 
     setContentView(R.layout.activity_calling_card);
 
-    mContentView = findViewById(R.id.fullscreen_content);
+    mContentView = findViewById(R.id.container);
 
-    mContactDetailsView = findViewById(R.id.contact_details);
-
-    addContactMethod(R.drawable.email, R.string.email);
-    addContactMethod(R.drawable.phone, R.string.phone);
-    addContactMethod(R.drawable.facebook, R.string.facebook);
-    addContactMethod(R.drawable.youtube, R.string.youtube);
-    addContactMethod(R.drawable.twitter, R.string.twitter);
-    addContactMethod(R.drawable.linkedin, R.string.linkedin);
+    if (savedInstanceState == null) {
+      getFragmentManager()
+          .beginTransaction()
+          .add(R.id.container, new CardFrontFragment())
+          .commit();
+    }
   }
 
-  private void addContactMethod(int imageResource, int textResource) {
-    LayoutInflater inflater = LayoutInflater.from(this);
-    View contactMethodView = inflater.inflate(R.layout.contact_method, null, false);
-
-    ImageView icon = (ImageView) contactMethodView.findViewById(R.id.icon);
-    icon.setImageResource(imageResource);
-
-    TextView text = (TextView) contactMethodView.findViewById(R.id.text);
-    text.setText(textResource);
-    text.setMovementMethod(LinkMovementMethod.getInstance());
-
-    ViewGroup contactDetailsViewGroup = (ViewGroup) mContactDetailsView;
-    int heightInPixels = (int) (36 * getResources().getDisplayMetrics().scaledDensity);
-
-    contactDetailsViewGroup.addView(contactMethodView, new ViewGroup.LayoutParams(LayoutParams.MATCH_PARENT, heightInPixels));
-  }
-  
   @Override
   protected void onPostCreate(Bundle savedInstanceState) {
     super.onPostCreate(savedInstanceState);
@@ -122,4 +109,81 @@ public class CallingCardActivity extends AppCompatActivity {
     mHideHandler.postDelayed(mHideRunnable, delayMillis);
     mHideHandler.postDelayed(mHideRunnable, delayMillis);
   }
+
+  @Override
+  public boolean onTouchEvent(MotionEvent event) {
+    flipCard();
+    return super.onTouchEvent(event);
+  }
+
+  private void flipCard() {
+    if (mShowingBack) {
+      getFragmentManager().popBackStack();
+      return;
+    }
+
+    mShowingBack = true;
+
+    getFragmentManager()
+        .beginTransaction()
+        .setCustomAnimations(
+            R.animator.card_flip_right_in,
+            R.animator.card_flip_right_out,
+            R.animator.card_flip_left_in,
+            R.animator.card_flip_left_out)
+        .replace(R.id.container, new CardBackFragment())
+        .addToBackStack(null)
+        .commit();
+  }
+
+  /**
+   * A fragment representing the front of the card.
+   */
+  public static class CardFrontFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState) {
+      return inflater.inflate(R.layout.fragment_card_front, container, false);
+    }
+  }
+
+  /**
+   * A fragment representing the back of the card.
+   */
+  public static class CardBackFragment extends Fragment {
+    private View mCardBackView;
+    private View mContactDetailsView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState) {
+      mCardBackView = inflater.inflate(R.layout.fragment_card_back, container, false);
+
+      mContactDetailsView = mCardBackView.findViewById(R.id.contact_details);
+
+      addContactMethod(inflater, R.drawable.email, R.string.email);
+      addContactMethod(inflater, R.drawable.phone, R.string.phone);
+      addContactMethod(inflater, R.drawable.facebook, R.string.facebook);
+      addContactMethod(inflater, R.drawable.youtube, R.string.youtube);
+      addContactMethod(inflater, R.drawable.twitter, R.string.twitter);
+      addContactMethod(inflater, R.drawable.linkedin, R.string.linkedin);
+
+      return mCardBackView;
+    }
+
+    private void addContactMethod(LayoutInflater inflater, int imageResource, int textResource) {
+      View contactMethodView = inflater.inflate(R.layout.contact_method, (ViewGroup) mCardBackView,false);
+
+      ImageView icon = (ImageView) contactMethodView.findViewById(R.id.icon);
+      icon.setImageResource(imageResource);
+
+      TextView text = (TextView) contactMethodView.findViewById(R.id.text);
+      text.setText(textResource);
+      text.setMovementMethod(LinkMovementMethod.getInstance());
+
+      ViewGroup contactDetailsViewGroup = (ViewGroup) mContactDetailsView;
+      int heightInPixels = (int) (36 * getResources().getDisplayMetrics().scaledDensity);
+
+      contactDetailsViewGroup.addView(contactMethodView, new ViewGroup.LayoutParams(LayoutParams.MATCH_PARENT, heightInPixels));
+    }  }
 }
