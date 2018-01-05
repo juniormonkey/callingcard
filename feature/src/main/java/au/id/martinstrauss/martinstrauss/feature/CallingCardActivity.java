b039a8b6f004450ccb2aff087e425d23f904c831
Martin Strauss <martinstrauss@google.com>
2018-01-05 13:21:11 -0800

    Programatically loop through the contact details to add them.

--- feature/src/main/java/au/id/martinstrauss/martinstrauss/feature/CallingCardActivity.java
+++ feature/src/main/java/au/id/martinstrauss/martinstrauss/feature/CallingCardActivity.java
@@ -5,8 +5,15 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.method.LinkMovementMethod;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -15,14 +22,7 @@ import android.view.View;
 public class CallingCardActivity extends AppCompatActivity {
 
   /**
-   * Whether or not the system UI should be auto-hidden after
-   * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-   */
-  private static final boolean AUTO_HIDE = true;
-
-  /**
-   * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-   * user interaction before hiding the system UI.
+   * The number of milliseconds to wait after user interaction before hiding the system UI.
    */
   private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
 
@@ -33,6 +33,7 @@ public class CallingCardActivity extends AppCompatActivity {
   private static final int UI_ANIMATION_DELAY = 300;
   private final Handler mHideHandler = new Handler();
   private View mContentView;
+  private View mContactDetailsView;
   private final Runnable mHidePart2Runnable = new Runnable() {
     @SuppressLint("InlinedApi")
     @Override
@@ -50,17 +51,6 @@ public class CallingCardActivity extends AppCompatActivity {
           | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
   };
-  private final Runnable mShowPart2Runnable = new Runnable() {
-    @Override
-    public void run() {
-      // Delayed display of UI elements
-      ActionBar actionBar = getSupportActionBar();
-      if (actionBar != null) {
-        actionBar.show();
-      }
-    }
-  };
-  private boolean mVisible;
   private final Runnable mHideRunnable = new Runnable() {
     @Override
     public void run() {
@@ -74,34 +64,33 @@ public class CallingCardActivity extends AppCompatActivity {
 
     setContentView(R.layout.activity_calling_card);
 
-    mVisible = true;
     mContentView = findViewById(R.id.fullscreen_content);
 
-    // Set up the user interaction to manually show or hide the system UI.
-    mContentView.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        toggle();
-      }
-    });
+    mContactDetailsView = findViewById(R.id.contact_details);
+
+    addContactMethod(R.drawable.email, R.string.email);
+    addContactMethod(R.drawable.phone, R.string.phone);
+    addContactMethod(R.drawable.facebook, R.string.facebook);
+    addContactMethod(R.drawable.youtube, R.string.youtube);
+    addContactMethod(R.drawable.twitter, R.string.twitter);
+    addContactMethod(R.drawable.linkedin, R.string.linkedin);
   }
 
-  @Override
-  protected void onPostCreate(Bundle savedInstanceState) {
-    super.onPostCreate(savedInstanceState);
+  private void addContactMethod(int imageResource, int textResource) {
+    LayoutInflater inflater = LayoutInflater.from(this);
+    View contactMethodView = inflater.inflate(R.layout.contact_method, null, false);
 
-    // Trigger the initial hide() shortly after the activity has been
-    // created, to briefly hint to the user that UI controls
-    // are available.
-    delayedHide(100);
-  }
+    ImageView icon = (ImageView) contactMethodView.findViewById(R.id.icon);
+    icon.setImageResource(imageResource);
 
-  private void toggle() {
-    if (mVisible) {
-      hide();
-    } else {
-      show();
-    }
+    TextView text = (TextView) contactMethodView.findViewById(R.id.text);
+    text.setText(textResource);
+    text.setMovementMethod(LinkMovementMethod.getInstance());
+
+    ViewGroup contactDetailsViewGroup = (ViewGroup) mContactDetailsView;
+    int heightInPixels = (int) (36 * getResources().getDisplayMetrics().scaledDensity);
+
+    contactDetailsViewGroup.addView(contactMethodView, new ViewGroup.LayoutParams(LayoutParams.MATCH_PARENT, heightInPixels));
   }
 
   private void hide() {
@@ -110,23 +99,9 @@ public class CallingCardActivity extends AppCompatActivity {
     if (actionBar != null) {
       actionBar.hide();
     }
-    mVisible = false;
 
     // Schedule a runnable to remove the status and navigation bar after a delay
-    mHideHandler.removeCallbacks(mShowPart2Runnable);
     mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-  }
-
-  @SuppressLint("InlinedApi")
-  private void show() {
-    // Show the system bar
-    mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-    mVisible = true;
-
-    // Schedule a runnable to display UI elements after a delay
-    mHideHandler.removeCallbacks(mHidePart2Runnable);
-    mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
   }
 
   /**
